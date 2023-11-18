15792079a3d6c93f5b7a6ac98be88e7048e5850d
andreymudri <andreybeckert@gmail.com>
2023-11-18 17:40:55 -0300

    Added: 4th exercise

--- Java_Duplicates/src/App.java
+++ Java_Duplicates/src/App.java
@@ -1,5 +1,13 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String[] array1 = {"maçã", "banana", "laranja", "uva", "pera"};
+        String[] array2 = { "abacaxi", "pêra", "uva", "limão", "banana" };
+        for (String item1 : array1) {
+            for (String item2 : array2) {
+                if (item1.equals(item2)) {
+                    System.out.println(item1);
+                }
+            }
+        }
     }
 }
